34b5787ff6f522eaa53e68938da26f776da4024d
hannarosenfeld <hannazrosenfeld@gmail.com>
2024-05-05 11:28:51 -0400

    Add store to my react app.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
+import { Provider } from 'react-redux'
 import configureStore from "./store";
 
 import './index.css'
@@ -10,10 +10,10 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-configureStore()
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={configureStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,10 @@
-// import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-// import thunk from 'redux-thunk';
-// import session from './session'
-// import userReducer from './user';
+import { configureStore } from '@reduxjs/toolkit'
+import session from './session'
+import userReducer from './user';
 
-// const rootReducer = combineReducers({
-//   session,
-//   user: userReducer,
-// });
-
-
-// let enhancer;
-
-// if (process.env.NODE_ENV === 'production') {
-//   enhancer = applyMiddleware(thunk);
-// } else {
-//   const logger = require('redux-logger').default;
-//   const composeEnhancers =
-//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-//   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
-// }
-
-// const configureStore = (preloadedState) => {
-//   return createStore(rootReducer, preloadedState, enhancer);
-// };
-
-const configureStore = () => {
-  console.log("❤️")
-};
-
-export default configureStore;
+export default configureStore({
+  reducer: {
+    session,
+    user: userReducer,
+  },
+})
