1f4b2ee06b0df1f68ace1d17de11bc349495de7b
hannarosenfeld <hannazrosenfeld@gmail.com>
2024-05-04 21:17:30 -0400

    Start hooking up store to app.

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+import configureStore from "./store";
+
 import './index.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+configureStore()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,31 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import session from './session'
-import userReducer from './user';
+// import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+// import thunk from 'redux-thunk';
+// import session from './session'
+// import userReducer from './user';
 
-const rootReducer = combineReducers({
-  session,
-  user: userReducer,
-});
+// const rootReducer = combineReducers({
+//   session,
+//   user: userReducer,
+// });
 
 
-let enhancer;
+// let enhancer;
 
-if (process.env.NODE_ENV === 'production') {
-  enhancer = applyMiddleware(thunk);
-} else {
-  const logger = require('redux-logger').default;
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   enhancer = applyMiddleware(thunk);
+// } else {
+//   const logger = require('redux-logger').default;
+//   const composeEnhancers =
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//   enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+// }
 
-const configureStore = (preloadedState) => {
-  return createStore(rootReducer, preloadedState, enhancer);
+// const configureStore = (preloadedState) => {
+//   return createStore(rootReducer, preloadedState, enhancer);
+// };
+
+const configureStore = () => {
+  console.log("❤️")
 };
 
 export default configureStore;
